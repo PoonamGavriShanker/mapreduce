c3baf6c00ce94d45ce124fa7728e4c99b4968a36
PoonamGavriShanker <poonam_shanker@seneca2.lehre.hwr-berlin.de>
2023-06-24 21:02:24 +0200

    Exercise: 4

--- mapper.py
+++ mapper.py
@@ -33,14 +33,15 @@ for line in sys.stdin:
     # strip removes whitespaces and new lines at the beginning and end of the line
     # the result is a tuple with 6 elements
     data = line.strip().split("\t")
-    if len(data) != 6:
-	raise ValueError("Please fill out all 6 elements.")
+    #if len(data) != 6:
+#	raise ValueError("Please fill out all 6 elements.")
 
     # store the 6 elements of the tuple in seperate variables
     date, time, item, category, sales, payment = data
-
+    
+    if category in ["Computers", "Cameras", "Video Games"]:
     # Write the key-value combination to standard output (stdout)
     # Key is the payment, value is the sales     
     # With a tab (\t) between key and value
     # New line \n means new record
-    sys.stdout.write("{0}\t{1}\n".format(category, sales))
+    	sys.stdout.write("{0}\t{1}\n".format(category, sales))
